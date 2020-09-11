d46a63ad3ebc9b9db7cad0182a9fcf4535e99461
SindhuRaghvan <44847368+SindhuRaghvan@users.noreply.github.com>
2020-09-11 12:57:31 -0700

    output update

--- kafkafiles/kafkaprocess.sh
+++ kafkafiles/kafkaprocess.sh
@@ -10,8 +10,6 @@ echo "Collecting Kafka Cluster information..."
 export clusterName=$(curl -u admin:$password -sS -G "http://headnodehost:8080/api/v1/clusters" | jq -r '.items[].Clusters.cluster_name')
 export KAFKAZKHOSTS=$(curl -sS -u admin:$password -G https://$clusterName.azurehdinsight.net/api/v1/clusters/$clusterName/services/ZOOKEEPER/components/ZOOKEEPER_SERVER | jq -r '["\(.host_components[].HostRoles.host_name):2181"] | join(",")' | cut -d',' -f1,2);
 export KAFKABROKERS=$(curl -sS -u admin:$password -G https://$clusterName.azurehdinsight.net/api/v1/clusters/$clusterName/services/KAFKA/components/KAFKA_BROKER | jq -r '["\(.host_components[].HostRoles.host_name):9092"] | join(",")' | cut -d',' -f1,2);
-echo $KAFKAZKHOSTS
-echo $KAFKABROKERS
 
 echo "Creating Kafka Topic...."
 /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --replication-factor 3 --partitions 8 --topic NewUser --zookeeper $KAFKAZKHOSTS
@@ -20,4 +18,7 @@ arrIN=(${KAFKABROKERS//,/ })
 
 echo "Updating producer file..."
 sed -i -e 's/<BROKER_HERE>/'$arrIN'/g' producer-simulator.py
+
+echo "Copy the following output to paste into the consumer file:"
+echo arrIN
 
